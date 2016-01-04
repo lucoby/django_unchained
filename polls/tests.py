560e6ef46605edb0378ea28a126764f09e64c3e3
Coby Lu <lucoby@gmail.com>
2016-01-04 23:39:35 +0000

    part 5 - more tests

--- polls/tests.py
+++ polls/tests.py
@@ -104,3 +104,39 @@ class QuestionViewTests(TestCase):
             response.context['latest_question_list'],
             ['<Question: Past question 2.>', '<Question: Past question 1.>']
         )
+        
+    def test_index_view_with_six_past_questions(self):
+        """
+        The questions index page may display multiple questions.
+        """
+        for i in range(1,6):
+            create_question(question_text="Past question " + str(i), days=-i)
+
+        response = self.client.get(reverse('polls:index'))
+        self.assertEqual(
+            len(response.context['latest_question_list']), 5
+        )
+
+class QuestionIndexDetailTests(TestCase):
+    def test_detail_view_with_a_future_question(self):
+        """
+        The detail view of a question with a pub_date in the future should
+        return a 404 not found.
+        """
+        future_question = create_question(question_text='Future question.',
+                                          days=5)
+        response = self.client.get(reverse('polls:detail',
+                                   args=(future_question.id,)))
+        self.assertEqual(response.status_code, 404)
+
+    def test_detail_view_with_a_past_question(self):
+        """
+        The detail view of a question with a pub_date in the past should
+        display the question's text.
+        """
+        past_question = create_question(question_text='Past Question.',
+                                        days=-5)
+        response = self.client.get(reverse('polls:detail',
+                                   args=(past_question.id,)))
+        self.assertContains(response, past_question.question_text,
+                            status_code=200)
